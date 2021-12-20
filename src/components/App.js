bff0ad43e496a049708e114f01f03dc8c5073430
Daniela <danisalazarcueto7@gmail.com>
2021-12-20 11:12:57 +0000

    installed sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,52 @@
-import "../styles/App.css";
+import "../styles/App.scss";
 
 function App() {
   return (
-    <div>
-      <h1>Hola Mundo 🌍</h1>
-    </div>
+    <>
+      <header className="header">
+        <h1>Calc</h1>
+        <h2>THEME</h2>                 
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+          <input type="checkbox" checked/>
+        
+      </header>
+      <section>
+        <input type="text" />
+      </section>
+      <section>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>DEL</button>
+      </section>
+      <section>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+      </section>
+      <section>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+        <label class="switch">
+          
+        </label>
+      </section>
+      <section>
+        <button>.</button>
+        <button>0</button>
+        <button>/</button>
+        <button>x</button>
+      </section>
+      <section>
+        <button>RESET</button>
+        <button>=</button>
+      </section>
+    </>
   );
 }
 

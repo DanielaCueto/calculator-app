3f2138fe6e17e800252bf2db5bc2432d0c969e93
Daniela <danisalazarcueto7@gmail.com>
2021-12-21 18:33:34 +0000

    add events to reset del and useState to everynumber

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,58 @@
+import { useState } from "react";
 import "../styles/App.scss";
 
+
 function App() {
+
+
+  const [number, setNumber] = useState('')
+
+  const clickNumber = (ev) =>{
+  const newNumber = number + ev.target.innerHTML;  
+  setNumber(newNumber); 
+  }
+
+  const handleDelete = () =>{
+    const newStatus = number.slice(0, number.length -1); 
+    setNumber(newStatus);
+  }
+
+  const handleReset = () =>{
+    setNumber(''); 
+  }
+
   return (
-    <>
+    <div className="App">
       <header className="header">
         <h1>Calc</h1>
-        <h2>THEME</h2>                 
+        <h2>THEME</h2>  
+                    
         <span>1</span>
         <span>2</span>
         <span>3</span>
           <input type="checkbox" checked/>
-        
       </header>
       <section>
-        <input type="text" />
+        <input type="number" name="name" value={number}/> 
       </section>
       <section>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>DEL</button>
+        <button onClick={clickNumber}>7</button>
+        <button onClick={clickNumber}>8</button>
+        <button onClick={clickNumber}>9</button>
+        <button onClick={handleDelete} >DEL</button>
       </section>
       <section>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
+        <button onClick={clickNumber}>4</button>
+        <button onClick={clickNumber}>5</button>
+        <button onClick={clickNumber}>6</button>
         <button>+</button>
       </section>
       <section>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>-</button>
-        <label class="switch">
+        <button onClick={clickNumber}>1</button>
+        <button onClick={clickNumber}>2</button>
+        <button onClick={clickNumber}>3</button>
+        <button >-</button>
+        <label>
           
         </label>
       </section>
@@ -43,10 +63,10 @@ function App() {
         <button>x</button>
       </section>
       <section>
-        <button>RESET</button>
+        <button onClick={handleReset}>RESET</button>
         <button>=</button>
       </section>
-    </>
+    </div>
   );
 }
 

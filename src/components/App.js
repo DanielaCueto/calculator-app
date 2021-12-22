cc6418b1ef660557f8516fb22eac2ce54ca36b18
Daniela <danisalazarcueto7@gmail.com>
2021-12-22 19:25:17 +0000

    add operations to calc app

--- src/components/App.js
+++ src/components/App.js
@@ -1,70 +1,118 @@
 import { useState } from "react";
 import "../styles/App.scss";
 
-
 function App() {
+  const [number, setNumber] = useState("0");
+  const [previousNumber, setPreviousNumber] = useState('0')
+  const [operation, setOperation] = useState ("")
+  const [firstDigit, setFirstDigit] = useState(true)
+  console.log(number, previousNumber, operation);
+  
+  
+  const handleNumber = (ev) => {
+    const newDigit = ev.target.innerHTML;
+    if(firstDigit){
+      setNumber(newDigit);
+    }else{
+      setNumber(number + newDigit);
+    }
+    setFirstDigit(false)
+    
+  };
 
-
-  const [number, setNumber] = useState('')
-
-  const clickNumber = (ev) =>{
-  const newNumber = number + ev.target.innerHTML;  
-  setNumber(newNumber); 
-  }
-
-  const handleDelete = () =>{
-    const newStatus = number.slice(0, number.length -1); 
+  const handleDelete = () => {
+    const newStatus = number.slice(0, number.length - 1);
+    if(newStatus === ""){
+      setNumber("0");
+      setFirstDigit(true)
+    }else{
     setNumber(newStatus);
+    }
+  };
+
+  const handleReset = () => {
+    setFirstDigit(true)
+    setNumber("0");
+    setPreviousNumber('0');
+    setOperation("");
+  };
+
+  const handleOperation = (ev) => {
+    
+    setPreviousNumber(number); 
+    setOperation(ev.target.innerHTML);
+    setFirstDigit(true)
+    
   }
 
-  const handleReset = () =>{
-    setNumber(''); 
+  const handleResult = () => {
+    const num1 = parseFloat(previousNumber);
+    const num2 =  parseFloat(number)
+    let lastResult;
+    if(operation === '+'){
+      lastResult = num1+num2 ;
+    }else if (operation === '-'){
+      lastResult = num1 - num2;
+      
+    } else if(operation === '/'){
+      lastResult = num1 / num2;
+    }
+  else if(operation === 'x'){
+    lastResult = num1 * num2;
+  }else{
+    return 
   }
+    setNumber(`${lastResult}`)
+    setPreviousNumber('0')
+    setOperation("")
+    setFirstDigit(true)
+    
+  }
+
+  
 
   return (
     <div className="App">
       <header className="header">
         <h1>Calc</h1>
-        <h2>THEME</h2>  
-                    
+        <h2>THEME</h2>
+
         <span>1</span>
         <span>2</span>
         <span>3</span>
-          <input type="checkbox" checked/>
+        <input type="checkbox" />
       </header>
       <section>
-        <input type="number" name="name" value={number}/> 
+        <input type="text" name="name" value={number} readOnly />
       </section>
       <section>
-        <button onClick={clickNumber}>7</button>
-        <button onClick={clickNumber}>8</button>
-        <button onClick={clickNumber}>9</button>
-        <button onClick={handleDelete} >DEL</button>
+        <button onClick={handleNumber}>7</button>
+        <button onClick={handleNumber}>8</button>
+        <button onClick={handleNumber}>9</button>
+        <button onClick={handleDelete}>DEL</button>
       </section>
       <section>
-        <button onClick={clickNumber}>4</button>
-        <button onClick={clickNumber}>5</button>
-        <button onClick={clickNumber}>6</button>
-        <button>+</button>
+        <button onClick={handleNumber}>4</button>
+        <button onClick={handleNumber}>5</button>
+        <button onClick={handleNumber}>6</button>
+        <button onClick={handleOperation}>+</button>
       </section>
       <section>
-        <button onClick={clickNumber}>1</button>
-        <button onClick={clickNumber}>2</button>
-        <button onClick={clickNumber}>3</button>
-        <button >-</button>
-        <label>
-          
-        </label>
+        <button onClick={handleNumber}>1</button>
+        <button onClick={handleNumber}>2</button>
+        <button onClick={handleNumber}>3</button>
+        <button onClick={handleOperation}>-</button>
+        
       </section>
       <section>
-        <button>.</button>
-        <button>0</button>
-        <button>/</button>
-        <button>x</button>
+        <button onClick={handleNumber} >.</button>
+        <button onClick={handleNumber}>0</button>
+        <button onClick={handleOperation} >/</button>
+        <button onClick={handleOperation}>x</button>
       </section>
       <section>
         <button onClick={handleReset}>RESET</button>
-        <button>=</button>
+        <button onClick={handleResult}>=</button>
       </section>
     </div>
   );

3c957dba92f56e35b6685ccb78be731367c7a8b2
Daniela <danisalazarcueto7@gmail.com>
2022-01-03 15:54:35 +0000

    changes in all files

--- src/components/KeyBoard.js
+++ src/components/KeyBoard.js
@@ -3,24 +3,24 @@ import "../styles/KeyBoard.scss"
 function KeyBoard (props) {
     return (
 <section className="keyboard" >
-<button onClick={props.handleNumber}>7</button>
-<button onClick={props.handleNumber}>8</button>
-<button onClick={props.handleNumber}>9</button>
-<button onClick={props.handleDelete}>DEL</button>
-<button onClick={props.handleNumber}>4</button>
-<button onClick={props.handleNumber}>5</button>
-<button onClick={props.handleNumber}>6</button>
-<button onClick={props.handleOperation}>+</button>
-<button onClick={props.handleNumber}>1</button>
-<button onClick={props.handleNumber}>2</button>
-<button onClick={props.handleNumber}>3</button>
-<button onClick={props.handleOperation}>-</button>
-<button onClick={props.handleNumber} >.</button>
-<button onClick={props.handleNumber}>0</button>
-<button onClick={props.handleOperation} >/</button>
-<button onClick={props.handleOperation}>x</button>
-<button onClick={props.handleReset}>RESET</button>
-<button onClick={props.handleResult}>=</button>
+<button onClick={props.handleNumber} className="keyboard__key">7</button>
+<button onClick={props.handleNumber} className="keyboard__key">8</button>
+<button onClick={props.handleNumber} className="keyboard__key">9</button>
+<button onClick={props.handleDelete} className="keyboard__key">DEL</button>
+<button onClick={props.handleNumber} className="keyboard__key">4</button>
+<button onClick={props.handleNumber} className="keyboard__key">5</button>
+<button onClick={props.handleNumber} className="keyboard__key">6</button>
+<button onClick={props.handleOperation} className="keyboard__key">+</button>
+<button onClick={props.handleNumber} className="keyboard__key">1</button>
+<button onClick={props.handleNumber} className="keyboard__key">2</button>
+<button onClick={props.handleNumber} className="keyboard__key">3</button>
+<button onClick={props.handleOperation} className="keyboard__key">-</button>
+<button onClick={props.handleNumber} className="keyboard__key">.</button>
+<button onClick={props.handleNumber} className="keyboard__key">0</button>
+<button onClick={props.handleOperation} className="keyboard__key">/</button>
+<button onClick={props.handleOperation} className="keyboard__key">x</button>
+<button onClick={props.handleReset} className="keyboard__key">RESET</button>
+<button onClick={props.handleResult} className="keyboard__key">=</button>
 </section>) }
 
 export default KeyBoard; 
